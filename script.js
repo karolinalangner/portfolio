3c03f6bba9f72fa57fb627e986de880ff16f762d
karolina langner <karolinalangner1@gmail.com>
2020-12-27 06:58:40 +0000

    new photo and some js added

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {respNavbar(), scrollPhoto()};
+const photo = document.getElementById("photo");
+const lewagon = document.getElementById("lewagon")
 
-function scrollFunction() {
+function respNavbar() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("navbar").style.padding = "30px 10px";
-    document.getElementById("navbar").style.backgroundColor = "white";
+    document.getElementById("navbar").style.backgroundImage = 'linear-gradient(rgb(255, 255, 255), rgba(255, 255, 255, 0.96))';
     document.getElementById("logo").style.fontSize = "35px";
     document.getElementById("links").style.fontSize = "10px";
   } else {
@@ -12,4 +14,10 @@ function scrollFunction() {
     document.getElementById("logo").style.fontSize = "70px";
     document.getElementById("links").style.fontSize = "20px";
   }
-}
+};
+
+function scrollPhoto() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    photo.style.opacity = "0.5";
+  }
+};
