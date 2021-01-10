ef67a70c2ff28c030bfa5f41dc49fd3bf44db769
karolina langner <karolinalangner1@gmail.com>
2021-01-10 23:37:20 +0000

    fully responsive

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function respNavbar() {
     document.getElementById("logo").style.fontSize = "35px";
     document.getElementById("links").style.fontSize = "10px";
   } else {
-    document.getElementById("navbar").style.padding = "50px 10px";
+    document.getElementById("navbar").style.padding = "30px 10px";
     document.getElementById("navbar").style.backgroundImage = "#linear-gradient(rgb(255, 255, 255), rgba(255, 255, 255, 0.96))";
     document.getElementById("logo").style.fontSize = "70px";
     document.getElementById("links").style.fontSize = "20px";
@@ -22,3 +22,30 @@ function scrollPhoto() {
     photo.style.opacity = "0.5";
   }
 };
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
